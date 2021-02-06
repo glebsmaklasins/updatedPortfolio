10ff95cff2f4325c0cc9f05e8e69a4859864a1a6
GlebsMaklasins <glebsmaklasins@gmail.com>
2021-02-06 22:57:34 +0000

    prepared everything for media queries

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -5,7 +5,7 @@ export default function Photo() {
   return (
 <div className="pictureBlock">
      <div className="mainPic">
-          <img src="./imgProfile/test_profile.jpg" alt="test" className="mainPic"/> 
+          <img src="./imgProfile/sidesmile.png" alt="test" className="mainPic"/> 
       </div>
         </div>
   )
